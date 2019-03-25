ef4a231424149d075a45ff44dd6280245ca4aa52
qq776355102 <41848512+qq776355102@users.noreply.github.com>
2019-03-25 11:32:20 +0800

    Update test.js

--- config/test.js
+++ config/test.js
@@ -15,9 +15,9 @@ var config = {
         db_prefix:'cvn_'//表前缀
     },
 
-    private_key: "c9e61d9cde31968fe881b0f6f9f6eabfed69a56f17f82da5d50d42016aaaf7a6",
+    private_key: "",
 
-    from_address: "0xeD4249236aa4ba1fD52f0643F0998324E73D719b",
+    from_address: "",
 
     //cvn提现配置
     cvn_tixian: {
